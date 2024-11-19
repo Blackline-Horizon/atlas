c4feca59648ccaea387ff14b47ce2dc793af4ce6
Sandip <sandipm02@icloud.com>
2024-11-18 17:42:31 -0700

    Spacing on comment

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 
 // Load environment variables based on the environment
- // have to set this on EC2 console like: export NODE_ENV=production
+// have to set this on EC2 console like: export NODE_ENV=production
 if (process.env.NODE_ENV === 'production') {
   dotenv.config({ path: path.join(__dirname, '../config/.env.prod') });
 } else {
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   // Start the app on the configured port
   await app.listen(port);
-  
+
   console.log(`Application is running on: http://localhost:${port}`);
 }
 
